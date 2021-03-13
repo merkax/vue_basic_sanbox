236812fb02c933fc621d462edbfdb70b42f12585
merkax <mishabigr@yandex.ru>
2021-03-14 00:48:28 +0300

    add sanbox for vue basic

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sunt asperiores, officia rem maiores consequatur esse minima sed ipsam aperiam! Earum doloremque perspiciatis tenetur perferendis id ex voluptates? Nisi, quidem.</p>
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
